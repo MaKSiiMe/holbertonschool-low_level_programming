08f75b9dccfc6d0f312a9ee24e46013534b2f7c3
MaKSiiMe <maxime.truel@gmail.com>
2024-10-21 09:10:59 +0000

    another one

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-int pust(const char *Programming is like building a multilingual puzzle);
-
 int main(){
-	puts();
+	puts(Programming is like building a multilingual puzzle);
 return(0);
 }
