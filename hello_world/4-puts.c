0812015e34b425b99a5520151afdae526ab32c0d
MaKSiiMe <maxime.truel@gmail.com>
2024-10-21 10:15:10 +0000

    bettystyle2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line".
+ * main - Entry point
  *
  * Return: Always 0
  */
