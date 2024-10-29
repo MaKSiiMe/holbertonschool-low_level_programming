8a9ca47bcf4e6e7d4faa80fef7f6070257bc6eb0
MaKSiiMe <maxime.truel@gmail.com>
2024-10-29 11:18:58 +0100

    bettystyle

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_triangle - Entry point
- * @size: variable to check
- * @n & @m: number to check
+ * reset_to_98 - Entry point
+ * @n: variable to check
  */
 
 void reset_to_98(int *n)
