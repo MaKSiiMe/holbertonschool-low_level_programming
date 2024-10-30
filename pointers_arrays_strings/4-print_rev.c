f880ef319ef74ddef5142bb71eef7624f2842cd4
MaKSiiMe <maxime.truel@gmail.com>
2024-10-30 14:10:33 +0100

    bettystyle

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,11 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _puts - Entry point
- * @str : char of string
- * Return: Always 1 or 0
+ * print_rev - Entry point
+ * @s: char of string
  */
 
 void print_rev(char *s)
-
 {
 	int l = 0;
 
@@ -17,7 +14,6 @@ void print_rev(char *s)
 		l++;
 		s++;
 	}
-
 	for (; l != 0; l--)
 	{
 		s--;
