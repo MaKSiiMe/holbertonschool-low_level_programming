38f4776a9c2f29480f9c2fa183d438b415592319
MaKSiiMe <maxime.truel@gmail.com>
2024-11-04 14:49:45 +0100

    3. strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strspn - Entry point
+ * @s: pointer
+ * @accept: string
+ * Return: @dest
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (j = 0; accept[j] != 0; j++)
+		{
+			if (s[i] == accept [j])
+				break;
+		}
+		if (s[i] != accept [j])
+			break;
+	}
+	return(i);
+}
