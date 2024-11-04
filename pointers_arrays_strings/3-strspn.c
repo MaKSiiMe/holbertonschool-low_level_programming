db9e9cb782cc66614943570fe84e09b4801cc0be
MaKSiiMe <maxime.truel@gmail.com>
2024-11-04 15:07:19 +0100

    bettystyle

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -15,11 +15,11 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != 0; j++)
 		{
-			if (s[i] == accept [j])
+			if (s[i] == accept[j])
 				break;
 		}
-		if (s[i] != accept [j])
+		if (s[i] != accept[j])
 			break;
 	}
-	return(i);
+	return (i);
 }
