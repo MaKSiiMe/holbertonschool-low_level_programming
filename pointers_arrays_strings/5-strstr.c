8139047989c862ca80c08a553975c0a06b32ee59
MaKSiiMe <maxime.truel@gmail.com>
2024-11-05 09:52:49 +0100

    5. strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strstr - Entry point
+ * @s: pointer
+ * @c: string
+ * Return: @dest
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j;
+
+	for (i = 0; haystack[i] != 0; i++)
+	{
+		for (j = 0; needle[j] != 0; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return ('\0');
+}
