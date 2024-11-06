58c7bbee2e8abc0ee909ffc0b3e3ab25ad9b7a46
MaKSiiMe <maxime.truel@gmail.com>
2024-11-06 14:34:57 +0100

    5. Your subconscious is looking for the dreamer

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Entry point
+ * @n: number
+ * Return: 1 or 0 or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	n = _sqrt(n, 1);
+	return (n);
+
+}
+
+/**
+ * _sqrt - Entry point
+ * @n: number
+ * @i: number
+ * Return: 1 or 0 or -1
+ */
+
+int _sqrt(int n, int i)
+{
+	if (n < 0 || i * i > n)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
