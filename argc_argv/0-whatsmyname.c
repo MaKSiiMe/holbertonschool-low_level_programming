fc1e2361744979a79ff996c23be9382146581171
MaKSiiMe <maxime.truel@gmail.com>
2024-11-07 10:08:50 +0100

    0. It ain't what they call you, it's what you answer to

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * @argc: 
+ * @argv: 
+ * Return: argv
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
