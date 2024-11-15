300e176794f3746c05452bba79135cb8286861fb
MaKSiiMe <maxime.truel@gmail.com>
2024-11-15 17:15:22 +0100

    incrementation

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,6 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,6 +21,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (arr == NULL)
 		return (NULL);
+
+	while (i < (nmemb * size))
+	{
+		arr[i] = 0;
+		i++;
+	}
 
 	return (arr);
 }
