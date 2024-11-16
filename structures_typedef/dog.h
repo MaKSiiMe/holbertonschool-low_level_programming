ed253a8b306893350b2cb9f19080d7794a5cd7ec
MaKSiiMe <maxime.truel@gmail.com>
2024-11-16 16:25:29 +0100

    0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Struct dog
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
