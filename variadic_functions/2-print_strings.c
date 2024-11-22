04ff0737ce1aed8796c548996eb1245b2d070f3c
MaKSiiMe <maxime.truel@gmail.com>
2024-11-22 10:03:59 +0100

    update va start

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	char *truc;
 
-	truc = va_arg(args, char*);
+	va_start(args, n);
 
 	for (; i < n; i++)
 	{
