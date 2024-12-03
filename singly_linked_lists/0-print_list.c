bea363f5297eca6bac4358960669b195c498627f
MaKSiiMe <maxime.truel@gmail.com>
2024-12-03 11:42:06 +0100

    0. Print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - Entry point
+ * @h: pointer to the first node
+ * Return: the number of node
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	
+	while(h)
+	{
+		if (h->str == NULL)
+			printf ("[0] (nil)\n");
+		else
+			printf ("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
