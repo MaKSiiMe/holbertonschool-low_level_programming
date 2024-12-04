743bd5a174a46a155eb9ae044eefe5a6223ac9f1
MaKSiiMe <maxime.truel@gmail.com>
2024-12-04 10:16:59 +0100

    macro

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
  * print_list - Entry point
@@ -10,13 +12,12 @@ size_t print_list(const list_t *h)
 {
 	int i = 0;
 
-	
-	while(h)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf ("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf ("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
